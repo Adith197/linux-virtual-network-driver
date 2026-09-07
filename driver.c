43448bfa9d62d65de8875d834df5acfd35a08c16
Adithyaa <adithyaa1907@gmail.com>
2026-09-07 19:27:59 +0530

    initiated virtual module

--- driver.c
+++ driver.c
@@ -6,15 +6,13 @@ MODULE_DESCRIPTION("Virtual Network Driver");
 
 static int __init vnet_init(void)
 {
-    //pr_info("vnet: module inserted\n");
-    printk("vnet inserted\n");
+    pr_info("vnet: module inserted\n");
     return 0;
 }
 
 static void __exit vnet_exit(void)
 {
-    //pr_info("vnet: module removed from kernel\n");
-    printk("vnet removed\n");
+    pr_info("vnet: module removed from kernel\n");
 }
 
 module_init(vnet_init);
